fb0179d93662f302762127660e61d08dd32291f8
Nicolas Girardin <ngirardin@gmail.com>
2014-03-10 21:19:53 +0100

    Fixing layout creation menu

--- app/src/main/java/fr/dmconcept/bob/activities/ProjectListActivity.java
+++ app/src/main/java/fr/dmconcept/bob/activities/ProjectListActivity.java
@@ -63,10 +63,11 @@ public class ProjectListActivity extends ListActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.project_list, menu);
-        return true;
+
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
