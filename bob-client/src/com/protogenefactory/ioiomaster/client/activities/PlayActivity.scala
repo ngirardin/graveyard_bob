aa95e90b58deb98df4f0451eebf9ad6098e97335
Nicolas Girardin <ngirardin@gmail.com>
2014-06-11 18:30:38 +0200

    Adding dummy PlayActivity

--- bob-client/src/com/protogenefactory/ioiomaster/client/activities/PlayActivity.scala
+++ bob-client/src/com/protogenefactory/ioiomaster/client/activities/PlayActivity.scala
@@ -1,5 +1,65 @@
 package com.protogenefactory.ioiomaster.client.activities
 
-class PlayActivity {
+import android.content.pm.ActivityInfo
+import org.scaloid.common._
+
+class PlayActivity extends SActivity {
+
+  val projects = Seq("Hello short", "Hello long", "Open hand", "Close hand", "Head left", "Head right")
+
+  val joysticks = Seq("Wheels", "Head", "Arm")
+
+  val mp3s = Seq("Hello!", "Bye-bye!", "Hi, I'm Bob!")
+
+  onCreate {
+
+    setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
+
+    contentView = new SLinearLayout {
+
+      this += new SVerticalLayout {
+
+        STextView("Sequences")
+
+        projects.map(p =>
+          SButton(p, toast(s"Play project $p"))
+            .fill
+        )
+      }.<<.wrap.Weight(1.0f).>>
+
+      this += new SVerticalLayout {
+
+        STextView("Joysticks")
+
+        joysticks.map(j =>
+          SToggleButton(j)
+            .textOff(j)
+            .textOn(j)
+            .fill
+        )
+      }.<<.wrap.Weight(1.0f).>>
+
+      this += new SVerticalLayout {
+
+        STextView("Sounds")
+
+        mp3s.map(m =>
+          SButton(m, toast(s"Play sound $m"))
+            .fill
+        )
+      }.<<.wrap.Weight(1.0f).>>
+
+      this += new SVerticalLayout {
+
+        SButton("VIDEO")
+          .<<(MATCH_PARENT, 100.dip).>>
+
+        STextView("Touch to disable video")
+
+      }.<<.wrap.Weight(2.0f).>>
+
+    }
+
+  }
 
 }
