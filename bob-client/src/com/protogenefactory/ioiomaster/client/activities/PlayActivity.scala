04f81483a9d1acc06ada2d49ce6c5f7eb8b66286
Nicolas Girardin <ngirardin@gmail.com>
2014-07-01 11:19:08 +0200

    Add scrollable button list on the play activity

--- bob-client/src/com/protogenefactory/ioiomaster/client/activities/PlayActivity.scala
+++ bob-client/src/com/protogenefactory/ioiomaster/client/activities/PlayActivity.scala
@@ -1,5 +1,7 @@
 package com.protogenefactory.ioiomaster.client.activities
 
+import java.util.{Timer, TimerTask}
+
 import android.media.MediaPlayer
 import android.media.MediaPlayer.{OnErrorListener, OnPreparedListener}
 import android.support.v4.widget.DrawerLayout
@@ -57,26 +59,28 @@ class PlayActivity extends SActivity {
         /**
          * Projects column
          */
-        this += new SVerticalLayout {
+        this += new SScrollView {
+          new SVerticalLayout {
 
-          STextView("Sequences")
-            .gravity(Gravity.CENTER)
-            .padding(0, HEADER_PADDING, 0, HEADER_PADDING)
+            STextView("Sequences")
+              .gravity(Gravity.CENTER)
+              .padding(0, HEADER_PADDING, 0, HEADER_PADDING)
 
-          projects.foreach(p =>
+            projects.foreach(p =>
 
-            this += new SButton(p.name) {
-              onClick({
-                val project = tag.asInstanceOf[Project]
-                PlayProgressDialog.show(context, project)
-                application.connection.playProject(project)
-              })
-            }
-            .tag(p)
-            .textSize(BUTTON_TEXT_SIZE.sp)
+              this += new SButton(p.name) {
+                onClick({
+                  val project = tag.asInstanceOf[Project]
+                  PlayProgressDialog.show(context, project)
+                  application.connection.playProject(project)
+                })
+              }
+                .tag(p)
+                .textSize(BUTTON_TEXT_SIZE.sp)
 
-          )
-        }.<<.wrap.Weight(1.0f).>>
+            )
+          }.<<.wrap.Weight(1.0f).>>
+        }
 
         /**
          * Joysticks column
@@ -98,40 +102,44 @@ class PlayActivity extends SActivity {
         /**
          * Sounds column
          */
-        this += new SVerticalLayout {
+        this += new SScrollView {
+          new SVerticalLayout {
 
-          STextView("Sounds")
-            .gravity(Gravity.CENTER)
-            .padding(0, HEADER_PADDING, 0, HEADER_PADDING)
+            STextView("Sounds")
+              .gravity(Gravity.CENTER)
+              .padding(0, HEADER_PADDING, 0, HEADER_PADDING)
 
-          mp3s.map(m =>
-            SButton(m, toast(s"Play sound $m"))
-              .textSize(BUTTON_TEXT_SIZE.sp)
-          )
-        }.<<.wrap.Weight(1.0f).>>
+            mp3s.map(m =>
+              SButton(m, toast(s"Play sound $m"))
+                .textSize(BUTTON_TEXT_SIZE.sp)
+            )
+          }.<<.wrap.Weight(1.0f).>>
+        }
 
         /**
          * Video column
          */
-        this += new SVerticalLayout {
+        this += new SScrollView {
+          new SVerticalLayout {
 
-          this += videoViewStatus
-            .padding(0, HEADER_PADDING, 0, HEADER_PADDING)
+            this += videoViewStatus
+              .padding(0, HEADER_PADDING, 0, HEADER_PADDING)
+              .gravity(Gravity.CENTER_HORIZONTAL)
+              .wrap.>>
+
+            this += videoView
+              .<<(VIDEO_WIDTH.dip, LayoutParams.WRAP_CONTENT).>>
+
+          }
             .gravity(Gravity.CENTER_HORIZONTAL)
-            .wrap.>>
-
-          this += videoView
-            .<<(VIDEO_WIDTH.dip, LayoutParams.WRAP_CONTENT).>>
-
+            .<<(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT).Weight(2.0f).>>
         }
-        .gravity(Gravity.CENTER_HORIZONTAL)
-        .<<(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT).Weight(2.0f).>>
 
       }
 
     )
 
-    val leftDrawer   = drawerLayout.find[ListView](R.id.left_drawer)
+    val leftDrawer = drawerLayout.find[ListView](R.id.left_drawer)
 
     leftDrawer.setAdapter(
       new ArrayAdapter[String](this, R.layout.drawer_list_item, Array("Home", "Projects", "News", "Special offers"))
@@ -149,51 +157,68 @@ class PlayActivity extends SActivity {
       }
     })
 
-    // Display the remote stream if any
-    def whatToString(what: Int) = what match {
-      case MediaPlayer.MEDIA_INFO_UNKNOWN               => "Unknown error"
-      case MediaPlayer.MEDIA_INFO_VIDEO_TRACK_LAGGING   => "Video track lagging"
-      case MediaPlayer.MEDIA_INFO_VIDEO_RENDERING_START => "Rendering start"
-      case MediaPlayer.MEDIA_INFO_BUFFERING_START       => "Buffering start"
-      case MediaPlayer.MEDIA_INFO_BUFFERING_END         => "Buffering end"
-      case MediaPlayer.MEDIA_INFO_BAD_INTERLEAVING      => "Bad interleaving"
-      case MediaPlayer.MEDIA_INFO_NOT_SEEKABLE          => "Not seekable"
-      case MediaPlayer.MEDIA_INFO_METADATA_UPDATE       => "Metadata update"
-      case MediaPlayer.MEDIA_INFO_UNSUPPORTED_SUBTITLE  => "Unsupported subtitle"
-      case MediaPlayer.MEDIA_INFO_SUBTITLE_TIMED_OUT    => "Timed out"
+    if (application.connection.hasVideo) {
+
+      // Display the remote stream if any
+      def whatToString(what: Int) = what match {
+        case MediaPlayer.MEDIA_INFO_UNKNOWN               => "Unknown error"
+        case MediaPlayer.MEDIA_INFO_VIDEO_TRACK_LAGGING   => "Video track lagging"
+        case MediaPlayer.MEDIA_INFO_VIDEO_RENDERING_START => "Rendering start"
+        case MediaPlayer.MEDIA_INFO_BUFFERING_START       => "Buffering start"
+        case MediaPlayer.MEDIA_INFO_BUFFERING_END         => "Buffering end"
+        case MediaPlayer.MEDIA_INFO_BAD_INTERLEAVING      => "Bad interleaving"
+        case MediaPlayer.MEDIA_INFO_NOT_SEEKABLE          => "Not seekable"
+        case MediaPlayer.MEDIA_INFO_METADATA_UPDATE => "Metadata update"
+        case MediaPlayer.MEDIA_INFO_UNSUPPORTED_SUBTITLE => "Unsupported subtitle"
+        case MediaPlayer.MEDIA_INFO_SUBTITLE_TIMED_OUT => "Timed out"
+      }
+
+      videoView.setOnErrorListener(new OnErrorListener {
+        override def onError(mp: MediaPlayer, what: Int, extra: Int): Boolean = {
+          val whatString = whatToString(what)
+          videoViewStatus.text(s"Streaming error: $whatString")
+          toast(s"Player error: what=$whatString, extra=$extra")
+          error(s"PlayActivity.videoView.onInfoListener() what=$whatString, extra=$extra")
+          true // The method has handled the error
+        }
+      })
+
+      /*
+      videoView.setOnInfoListener(new OnInfoListener {
+        override def onInfo(mp: MediaPlayer, what: Int, extra: Int): Boolean = {
+          toast(s"Play info: what=$what, extra=$extra")
+          info(s"PlayActivity.videoView.onInfoListener() what=$what, extra=$extra")
+          true // The method has handled the info
+        }
+      })
+      */
+
+      videoView.setOnPreparedListener(new OnPreparedListener {
+        override def onPrepared(mp: MediaPlayer) {
+          //TODO remove
+          toast("Starting streaming")
+
+          new Timer().schedule(new TimerTask() {
+            override def run() {
+              info(s"-------------------------- Position: ${videoView.getCurrentPosition}/${videoView.getDuration} [${videoView.getBufferPercentage}%]")
+            }
+          }, 0, 200)
+
+          videoViewStatus.text("Remote video")
+          info(s"PlayActivity.videoView.onPreparedListener() Ready for playback")
+        }
+      })
+
+
+        videoView.setVideoURI(s"rtsp://${application.serverIP}:8086/")
+//      videoView.setVideoURI("rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov")
+      //    videoView.setVideoURI("/sdcard/DCIM/Camera/VID_20140630_110902.mp4")
+
+      videoView.start()
+
+    } else {
+      videoViewStatus.text("Remote video only available\nduring remote connection")
     }
-    // ---- TODO use remote IP
-    videoView.setOnErrorListener(new OnErrorListener {
-      override def onError(mp: MediaPlayer, what: Int, extra: Int): Boolean = {
-        val whatString = whatToString(what)
-        videoViewStatus.text(s"Streaming error: $whatString")
-        toast(s"Player error: what=$whatString, extra=$extra")
-        error(s"PlayActivity.videoView.onInfoListener() what=$whatString, extra=$extra")
-        true // The method has handled the error
-      }
-    })
-
-    /*
-    videoView.setOnInfoListener(new OnInfoListener {
-      override def onInfo(mp: MediaPlayer, what: Int, extra: Int): Boolean = {
-        toast(s"Play info: what=$what, extra=$extra")
-        info(s"PlayActivity.videoView.onInfoListener() what=$what, extra=$extra")
-        true // The method has handled the info
-      }
-    })
-    */
-
-    videoView.setOnPreparedListener(new OnPreparedListener {
-      override def onPrepared(mp: MediaPlayer) {
-        videoViewStatus.text("Remote video")
-        info(s"PlayActivity.videoView.onPreparedListener() Ready for playback")
-      }
-    })
-
-//    videoView.setVideoURI("rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov")
-//    videoView.setVideoURI("rtsp://192.168.0.43:8086/")
-    videoView.setVideoURI("/sdcard/DCIM/Camera/VID_20140630_110902.mp4")
-    videoView.start()
 
     contentView = drawerLayout
 
