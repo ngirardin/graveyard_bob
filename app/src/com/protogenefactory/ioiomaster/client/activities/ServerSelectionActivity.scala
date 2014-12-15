2fd1cda622cd120e0850ec1433a7f8d00d764012
Nicolas Girardin <ngirardin@gmail.com>
2014-12-15 17:35:07 +0100

    Moving the server selection layout to an XML file

--- app/src/com/protogenefactory/ioiomaster/client/activities/ServerSelectionActivity.scala
+++ app/src/com/protogenefactory/ioiomaster/client/activities/ServerSelectionActivity.scala
@@ -3,58 +3,112 @@ package com.protogenefactory.ioiomaster.client.activities
 import java.util.Date
 import java.util.regex.Pattern
 
-import android.view.Gravity
+import android.app.Activity
+import android.content.DialogInterface
+import android.content.DialogInterface.OnClickListener
+import android.os.Bundle
+import android.text.{Editable, TextWatcher}
+import android.view.{View, Gravity}
 import android.view.inputmethod.EditorInfo
+import android.widget.{EditText, LinearLayout, Button, TextView}
+import com.protogenefactory.ioiomaster.R
 import com.protogenefactory.ioiomaster.client.BobApplication
 import com.protogenefactory.ioiomaster.server.services.ServerService
 import org.scaloid.common._
 
 class ServerSelectionActivity extends SActivity {
 
-  implicit override val loggerTag = LoggerTag("Bob")
+  lazy val application   = getApplication.asInstanceOf[BobApplication]
+  lazy val serverService = new LocalServiceConnection[ServerService]
 
-  final val IP_REGEXP = Pattern.compile("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
+  override def onCreate(savedInstanceState: Bundle): Unit = {
 
-  lazy val application = getApplication.asInstanceOf[BobApplication]
+    super.onCreate(savedInstanceState)
 
-  val serverService = new LocalServiceConnection[ServerService]
+    setContentView(R.layout.activity_server_selection)
 
-  lazy val buttonConnectLocal: SButton = new  SButton("Use local IOIO board") {
-    onClick({
+    serverService.run(service => {
 
-      application.setLocalConnection(serverService)
+      info(s"ServerSelectionActivity.buttonConnectLocal Local connection available: ${service.isIOIOStarted}")
 
-      startActivity[PlayActivity]
+      if (service.isIOIOStarted) {
 
-      // Remove itself from the stack
-      finish()
+        findViewById(R.id.layoutLocal).asInstanceOf[LinearLayout].setVisibility(View.VISIBLE)
+        setupListenersLocalBoard()
+
+      } else {
+
+        findViewById(R.id.layoutRemote).asInstanceOf[LinearLayout].setVisibility(View.VISIBLE)
+        setupListenersRemoteBoard()
+
+      }
+
     })
+
+    updateVersion()
+
   }
 
-  lazy val editTextIP : SEditText = new SEditText("Server IP address") {
+  private def setupListenersLocalBoard(): Unit = {
+
+    findViewById(R.id.buttonConnectLocal).asInstanceOf[Button].setOnClickListener(new OnClickListener {
+
+      override def onClick(dialog: DialogInterface, which: Int): Unit = {
+        application.setLocalConnection(serverService)
+        startActivity[PlayActivity]
+        // Remove itself from the stack
+        finish()
+      }
+
+    })
+
+  }
+
+  private def setupListenersRemoteBoard(): Unit = {
+
+    val IP_REGEXP = Pattern.compile("^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$")
+
+    val editTextIP          = findViewById(R.id.editTextIP         ).asInstanceOf[EditText]
+    val buttonConnectRemote = findViewById(R.id.buttonConnectRemote).asInstanceOf[Button  ]
 
     // Avoid full screen keyboard on landscape
-    setImeOptions(EditorInfo.IME_FLAG_NO_EXTRACT_UI)
+    editTextIP.setImeOptions(EditorInfo.IME_FLAG_NO_EXTRACT_UI)
 
-    hint("IP address")
+    editTextIP.hint("IP address")
 
-    text(application.serverIP)
+    editTextIP.text(application.serverIP)
 
-    onTextChanged({ (text: CharSequence, start: Int, lenghtBefore: Int, lenghtAfter: Int) =>
+    editTextIP.addTextChangedListener(new TextWatcher() {
 
-      val valid = text.length() > 0 && IP_REGEXP.matcher(text.toString).matches()
+      override def beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) : Unit ={
+        // Nothing
+      }
 
-      if (!valid)
-        setError("Invalid IP address")
+      override def onTextChanged(s: CharSequence, start: Int, before: Int, count: Int): Unit = {
 
-      buttonConnectRemote.setEnabled(valid)
+        val valid = s.length() > 0 && IP_REGEXP.matcher(s.toString).matches()
+
+        if (!valid)
+          editTextIP.setError("Invalid IP address")
+
+        buttonConnectRemote.setEnabled(valid)
+
+      }
+
+      override def afterTextChanged(s: Editable): Unit = {
+        // Nothing
+      }
 
     })
 
-  }
+    // Enable the button if an IP address is stored in the preferences
+    if (application.serverIP.nonEmpty)
+      buttonConnectRemote.setEnabled(true)
 
-  lazy val buttonConnectRemote : SButton = new SButton("Connect") {
-    onClick({
+    buttonConnectRemote.onClick {
+
+      // Disable the Connect button while connecting
+      buttonConnectRemote.setEnabled(false)
 
       val ip = editTextIP.getText.toString
 
@@ -66,56 +120,23 @@ class ServerSelectionActivity extends SActivity {
         finish()
       } else {
         alert(s"Can't connect to the app", s"Unable to connect to $ip, check that the IOIO is connected on the other device.")
+        // Re-enable the Connect button
+        buttonConnectRemote.setEnabled(true)
       }
 
-    })
-  }.enabled(!application.serverIP.isEmpty)
-
-  lazy val layoutConnectRemote = new SLinearLayout {
-    this += new STextView("Remote IP")
-    this += editTextIP.<<(150.dip, WRAP_CONTENT).>>
-    this += buttonConnectRemote
-  }
-    .gravity(Gravity.CENTER_HORIZONTAL)
-
-  onCreate {
-
-    info("ServerSelectionActivity.onCreate()")
-
-    contentView = new SVerticalLayout {
-
-      serverService.run(service => {
-
-        info(s"ServerSelectionActivity.buttonConnectLocal Local connection available: ${service.isIOIOStarted}")
-
-        if (service.isIOIOStarted) {
-
-          this += new SVerticalLayout {
-            this += buttonConnectLocal.<<(250.dip, WRAP_CONTENT).>>
-          }
-            .<<.Weight(1).>>
-            .gravity(Gravity.CENTER)
-
-        } else {
-
-          this += new SVerticalLayout {
-            this += layoutConnectRemote.wrap.>>
-          }
-            .<<.Weight(1).>>
-            .gravity(Gravity.CENTER)
-
-        }
-
-        val pm        = getPackageManager.getPackageInfo(getPackageName, 0)
-        val version   = pm.versionName
-        val buildDate = new Date(pm.lastUpdateTime)
-
-        STextView(s"Version $version\nBuilt $buildDate")
-          .gravity(Gravity.CENTER_HORIZONTAL)
-
-      })
-
     }
+
+  }
+
+  private def updateVersion() {
+
+    val textViewVersion = findViewById(R.id.textViewVersion).asInstanceOf[TextView]
+
+    val pm        = getPackageManager.getPackageInfo(getPackageName, 0)
+    val version   = pm.versionName
+    val buildDate = new Date(pm.lastUpdateTime)
+
+    textViewVersion.setText(s"Version $version\nBuilt $buildDate")
 
   }
 
